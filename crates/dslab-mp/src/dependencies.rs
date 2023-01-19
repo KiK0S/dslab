bcfe98c5c77128523cbe038dc9ef603441cfb781
Konstantin Amelichev <kostya.amelichev@gmail.com>
2023-01-20 00:44:21 +0400

    timer dependency resolver: pop used events,

--- crates/dslab-mp/src/dependencies.rs
+++ crates/dslab-mp/src/dependencies.rs
@@ -10,14 +10,15 @@ use std::rc::Rc;
 use std::vec::Vec;
 
 #[derive(Debug)]
-pub struct DependencyWrapper<T: Copy + PartialEq + Debug> {
+struct DependencyWrapper<T: Copy + PartialEq + Debug> {
     pub inner: T,
     dependencies_before: Vec<Rc<RefCell<DependencyWrapper<T>>>>,
     dependencies_after: Vec<Rc<RefCell<DependencyWrapper<T>>>>,
 }
 
-pub struct TimerDependencyResolver {
+struct TimerDependencyResolver {
     node_timers: HashMap<Id, Vec<(f64, Rc<RefCell<DependencyWrapper<EventId>>>)>>,
+    event_to_node: HashMap<EventId, Id>,
 }
 
 pub struct DependencyResolver {
@@ -29,9 +30,11 @@ impl TimerDependencyResolver {
     pub fn new() -> Self {
         TimerDependencyResolver {
             node_timers: HashMap::new(),
+            event_to_node: HashMap::new(),
         }
     }
     pub fn add(&mut self, node: Id, time: f64, event: Rc<RefCell<DependencyWrapper<EventId>>>) {
+        assert!(self.event_to_node.insert(event.as_ref().borrow().inner, node).is_none(), "duplicate EventId not allowed");
         let timers = self.node_timers.entry(node).or_default();
         let mut max_time_before_idx = None;
         if let Some(first_timer) = timers.first() {
@@ -70,9 +73,27 @@ impl TimerDependencyResolver {
         }
     }
 
-    pub fn pop(&mut self, _node: Id) {
-        // TODO: need some future implementation
-        assert!(false);
+    pub fn pop(&mut self, event_id: EventId) {
+        let node = self.event_to_node.remove(&event_id).unwrap();
+        let data = self.node_timers.get_mut(&node).unwrap();
+        assert!(data.len() > 0, "cannot pop from empty vector");
+        let mut event_pos = None;
+        for (idx, elem) in data.iter().enumerate() {
+            if elem.1.as_ref().borrow().inner == event_id {
+                event_pos = Some(idx);
+            }
+        }
+        assert!(event_pos.is_some());
+        let event_pos = event_pos.unwrap();
+        assert!(data[event_pos].0 == data[0].0);
+        if event_pos > 0 && event_pos + 1 < data.len() {
+            // need to link over removed elem
+            let a = &data[event_pos - 1].1;
+            let b = &data[event_pos + 1].1;
+            a.as_ref().borrow_mut().add_child(&b);
+            b.as_ref().borrow_mut().add_parent(&a);
+        }
+        data.remove(event_pos);
     }
 }
 
@@ -110,9 +131,8 @@ impl DependencyResolver {
             .available_events
             .iter()
             .enumerate()
-            .find(|x| x.1.as_ref().borrow().inner == event_id);
-        assert!(event.is_some());
-        let next_events = event.unwrap().1.as_ref().borrow_mut().pop_dependencies();
+            .find(|x| x.1.as_ref().borrow().inner == event_id).unwrap();
+        let next_events = event.1.as_ref().borrow_mut().pop_dependencies();
         for dependency in next_events.iter() {
             let idx = dependency
                 .as_ref()
@@ -120,17 +140,18 @@ impl DependencyResolver {
                 .dependencies_before
                 .iter()
                 .enumerate()
-                .find(|elem| elem.1.as_ref().borrow().inner == event.unwrap().1.as_ref().borrow().inner)
+                .find(|elem| elem.1.as_ref().borrow().inner == event.1.as_ref().borrow().inner)
                 .unwrap()
                 .0;
             dependency.borrow_mut().dependencies_before.remove(idx);
         }
-        self.available_events.remove(event.unwrap().0);
+        self.available_events.remove(event.0);
         for dependency in next_events.iter() {
             if dependency.as_ref().borrow().is_available() {
                 self.available_events.push(dependency.clone());
             }
         }
+        self.timer_resolver.pop(event_id);
     }
 }
 
@@ -195,3 +216,53 @@ fn test_dependency_resolver_simple() {
         timers[node as usize] += 1;
     }
 }
+
+#[test]
+fn test_dependency_resolver_pop() {
+    let mut resolver = DependencyResolver::new();
+    let mut sequence = Vec::new();
+    for node_id in 0..3 {
+        let mut times: Vec<u64> = (0..3).into_iter().collect();
+        times.shuffle(&mut rand::thread_rng());
+        for event_time in times {
+            let event = Event {
+                id: event_time * 3 + node_id,
+                src: node_id as u32,
+                dest: 0,
+                time: event_time as f64,
+                data: Box::new(SamplePayload {}),
+            };
+            resolver.add_event(&event);
+        }
+    }
+    for _ in 0..7 {
+        let id = *resolver.available_events().choose(&mut rand::thread_rng()).unwrap();
+        sequence.push(id);
+        resolver.pop_event(id);
+    }
+    for node_id in 0..3 {
+        let event = Event {
+            id: 9 + node_id,
+            src: node_id as u32,
+            dest: 0,
+            time: 3.0,
+            data: Box::new(SamplePayload {}),
+        };
+        resolver.add_event(&event);
+    }
+    while let Some(id) = resolver.available_events().choose(&mut rand::thread_rng()) {
+        let id = *id;
+        sequence.push(id);
+        resolver.pop_event(id);
+    }
+    println!("{:?}", sequence);
+    assert!(sequence.len() == 12);
+    let mut timers = vec![0, 0, 0];
+    for event_id in sequence {
+        let time = event_id / 3;
+        let node = event_id % 3;
+        assert!(timers[node as usize] == time);
+        timers[node as usize] += 1;
+    }
+}
+
